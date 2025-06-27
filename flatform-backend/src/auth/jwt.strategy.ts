6c198d1eb048a737d876b5c4f1c524b667d956f2
Khang Huynh <khangover27@gmail.com>
2025-06-27 22:41:03 +0700

    Fixed login page

--- flatform-backend/src/auth/jwt.strategy.ts
+++ flatform-backend/src/auth/jwt.strategy.ts
@@ -7,7 +7,9 @@ import { ConfigService } from '@nestjs/config';
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(configService: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req) => req?.cookies?.token,
+      ]),
       ignoreExpiration: false,
       secretOrKey: configService.get<string>('JWT_SECRET')!,
     });
