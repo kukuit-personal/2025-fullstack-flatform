d4f311244203b159c82c47d0b4455649dd8347f2
Khang Huynh <khangover27@gmail.com>
2025-08-22 14:30:01 +0700

    BE: move /users to admin/users, add alias for prisma

--- flatform-backend/src/app.module.ts
+++ flatform-backend/src/app.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
-import { APP_GUARD } from '@nestjs/core'
-import { RolesGuard } from './common/guards/roles.guard'
-import { JwtAuthGuard } from './common/guards/jwt-auth.guard'
-import { CombinedAuthGuard } from './common/guards/combined-auth.guard'
-import { AuthModule } from './modules/auth/auth.module'
-import { UsersModule } from './modules/users/users.module'
-import { NotedModule } from './modules/noted/noted.module'
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './common/guards/roles.guard';
+import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
+import { CombinedAuthGuard } from './common/guards/combined-auth.guard';
+import { AuthModule } from './modules/auth/auth.module';
+import { NotedModule } from './modules/noted/noted.module';
+import { UsersModule } from './modules/admin/users/users.module';
 
 @Module({
   imports: [
